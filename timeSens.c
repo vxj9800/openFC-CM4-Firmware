9cc9b46c7c74248c8c8ebfab17828eb12d370dea
Vatsal Joshi <vatsalasitkumar.joshi@mavs.uta.edu>
2023-05-30 14:25:31 -0500

    Split timeSens into two sensors
    This way, dma can direct take value from the timerlr and timerhr
    registers. This method will be computationally less expensive
    for the microcontroller.

--- timeSens.c
+++ timeSens.c
@@ -1,8 +1,7 @@
 #include "timeSens.h"
 
 // Data Definitions
-static uint64_t time;
-static bool timeSens_drdy = false;
+static bool timeSensLo_drdy = true, timeSensHi_drdy = true;
 
 // Repeating timer global for calling the update data function repeatedly
 static repeating_timer_t t_timeSens;
@@ -13,15 +12,13 @@ void timeSens_setup()
     add_repeating_timer_us(1000, timeSens_update_data, NULL, &t_timeSens);
 
     // Register the timeSens with the usb data communication system
-    register_sensor(0, DTYPE_UINT64, 1, 1000, (uint8_t *)&time, &timeSens_drdy);
+    register_sensor(0, DTYPE_UINT32, 1, 1000, (uint8_t *)&timer_hw->timelr, &timeSensLo_drdy);
+    register_sensor(1, DTYPE_UINT32, 1, 1000, (uint8_t *)&timer_hw->timehr, &timeSensHi_drdy);
 }
 
 bool timeSens_update_data(repeating_timer_t *tmr)
 {
-    if (!timeSens_drdy)
-    {
-        time = time_us_64();
-        timeSens_drdy = true;
-    }
+    timeSensLo_drdy = true;
+    timeSensHi_drdy = true;
     return 1;
 }
